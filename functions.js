3ce71a0a5f866e257fd3ee3f8b5a564e2fecaa1b
Alberto Liguori <a1liguorialberto@gmail.com>
2020-10-30 00:32:41 +0100

    change

--- functions.js
+++ functions.js
@@ -6,9 +6,11 @@ function randomBetween(min, max){
 }
 
 //Funzione comparazione
-function isNumberInArray(num,numArr){
-  for(var i=0; i < numArr.length;i++){
-    return (num===numArr[i])
+function isNumberInArray(num,numArray){
+  for(var i=0; i < numArray.length;i++){
+    if (num===numArray[i]){
+      return true
+    }
   }
 }
 
@@ -32,3 +34,62 @@ function initGame(){
   console.log(fieldArray)
 
 }
+
+// //Genera bottoni
+// function htmlElGen(anArray, htmlEl){
+//   for (i=0; i< anArray.length; i++){
+//     htmlEl.innerHTML+=`<li> <button onclick="document.getElementById('step').value=this.value" id="`  +i+ `" value="` + i + `">` + anArray[i] + `</button> </li>`
+//   }
+// }
+
+
+
+// //Genera bottoni
+// function htmlElGen(anArray, htmlEl){
+//   for (i=0; i< anArray.length; i++){
+//     htmlEl.innerHTML+=`<li> <button onclick="document.getElementById('step').value=this.value" id="`  +i+ `" value="` + i + `">` + anArray[i] + `</button> </li>`
+//   }
+// }
+
+//Genera bottoni
+function htmlElGen(anArray, htmlEl){
+  for (i=0; i< anArray.length; i++){
+    htmlEl.innerHTML+=`<li> <button onclick="play(this.value)" id="`  +i+ `" value="` + i + `">` + anArray[i] + `</button> </li>`
+  }
+}
+
+
+
+function play(stepOnField){
+
+  stepOnField = parseInt(stepOnField)
+
+  console.log(stepOnField)
+
+  if (!(fieldArray.includes(stepOnField))){
+    fieldArray.push(stepOnField)
+    scoreCounter++;
+    stepsToVictory--;
+    document.getElementById("message").innerHTML="continua così!";
+    document.getElementById("yourSteps").innerHTML+="<li>"+stepOnField+"</li>";
+    document.getElementById("stepsToVictory").innerHTML="Ti mancano " + stepsToVictory + " passi alla vittoria";
+    document.getElementById("score").innerHTML="Punteggio:" + scoreCounter
+  } else{
+    scoreCounter= 0;
+    document.getElementById("yourSteps").innerHTML="";
+    document.getElementById("message").innerHTML="ka-boom";
+    document.getElementById("score").innerHTML="Punteggio:" + scoreCounter;
+    fieldArray=[];
+  }
+
+  console.log(fieldArray)
+
+}
+
+
+// //Genera bottoni
+// function htmlElGen(anArray, htmlEl){
+//   for (i=0; i< anArray.length; i++){
+//     htmlEl.innerHTML+=`<li> <button onclick="play(this.value)" id="`  +i+ `" value="` + i + `">` + anArray[i] + `</button> </li>`
+//   }
+// }
--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 // OK // Il computer deve generare 16 numeri casuali tra 1 e 100.
 // OK I numeri non possono essere duplicati
 
-// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// OK In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 
-// L'utente non può inserire più volte lo stesso numero.
+// OK L'utente non può inserire più volte lo stesso numero.
 
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero "vietato" o raggiunge il numero massimo possibile di numeri consentiti.
@@ -17,51 +17,55 @@
 // con difficoltà 1 =>  tra 1 e 80
 // con difficoltà 2 => tra 1 e 50
 
+
+//Dichiarazione delle variabili
 var fieldArray=arrayGenerator(16, 100);
 console.log(fieldArray)
-
 var scoreCounter=0;
 var stepsToVictory = 0;
 
-// Selezione della difficoltà e Gioca
+// Seleziona la difficoltà e Gioca
 document.getElementById("modeSelection").addEventListener("change", function(){
 
   initGame()
 
 })
 
-//Inizializza una nuova giocata
+//Inizializza una nuova giocata con la difficoltà selezionata in precedenza
 document.getElementById("newGame").addEventListener("click", function(){
 
   initGame()
 
 })
 
-document.getElementById("click").addEventListener("click", function(){
+//Azione Gioca
+// document.getElementById("step").addEventListener("input", function(){
+//
+//   var stepOnField = parseInt(document.getElementById("step").value)
+//   // document.getElementById("step").value="";
+//   console.log(stepOnField)
+//
+//   if (!(fieldArray.includes(stepOnField))){
+//     fieldArray.push(stepOnField)
+//     scoreCounter++;
+//     stepsToVictory--;
+//     document.getElementById("message").innerHTML="continua così!";
+//     document.getElementById("yourSteps").innerHTML+="<li>"+stepOnField+"</li>";
+//     document.getElementById("stepsToVictory").innerHTML="Ti mancano " + stepsToVictory + " passi alla vittoria";
+//     document.getElementById("score").innerHTML="Punteggio:" + scoreCounter
+//   } else{
+//     scoreCounter= 0;
+//     document.getElementById("yourSteps").innerHTML="";
+//     document.getElementById("message").innerHTML="ka-boom";
+//     document.getElementById("score").innerHTML="Punteggio:" + scoreCounter;
+//     fieldArray=[];
+//   }
+//
+//   console.log(fieldArray)
+//
+// })
 
-  var stepOnField = parseInt(document.getElementById("step").value)
-  document.getElementById("step").value="";
-  console.log(stepOnField)
 
-  if (!(fieldArray.includes(stepOnField))){
-    fieldArray.push(stepOnField)
-    scoreCounter++;
-    stepsToVictory--;
-    document.getElementById("message").innerHTML="continua così!";
-    document.getElementById("yourSteps").innerHTML+="<li>"+stepOnField+"</li>";
-    document.getElementById("stepsToVictory").innerHTML="Ti mancano " + stepsToVictory + " passi alla vittoria";
-    document.getElementById("score").innerHTML="Punteggio:" + scoreCounter
-  } else{
-    scoreCounter= 0;
-    document.getElementById("yourSteps").innerHTML="";
-    document.getElementById("message").innerHTML="ka-boom";
-    document.getElementById("score").innerHTML="Punteggio:" + scoreCounter;
-    fieldArray=[];
-  }
-
-
-  console.log(fieldArray)
-
-
-
-})
+var inputListArray = arrayGenerator(100, 100)
+var lista = document.getElementById("lista")
+htmlElGen(inputListArray, lista)
